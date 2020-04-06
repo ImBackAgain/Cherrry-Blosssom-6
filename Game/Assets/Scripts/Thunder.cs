9722d9f6517b11428649e2fd84d60922c2970669
Enan Munzar (Feret) <Kamenwati333@gmail.com>
2020-04-06 00:18:19 -0400

    Sound work begun
    Placeholder music, the beginnnings of an ambience, and an atttempt at a
    UI sound are present. Lightning has beeen implemented, with a
    rudimentary thunder sound in place. There are many random numbers.

---
+++ Game/Assets/Scripts/Thunder.cs
@@ -0,0 +1,130 @@
+﻿using FMOD.Studio;
+using FMODUnity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thunder : MonoBehaviour
+{
+    float timer;
+    bool inProgresss = false;
+    [SerializeField] [FMODUnity.EventRef] string thunderEvent;
+    const float MAX_INTENSITY = 3.2f;
+    Light light;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HowLongTillLight();
+        light = GetComponent<Light>();
+    }
+
+    void HowLongTillLight()
+    {
+        timer = Random.Range(5, 12f);
+        //print(timer);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inProgresss) return; //Note: this is is only for lightning. Thunder can happpen in multiple instances concurrrently.
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            inProgresss = true;
+            StartCoroutine(Lightning());
+        }
+    }
+
+    void RandomDirect()
+    {
+        float xRot = Random.Range(10, 90f);
+        float yRot = Random.Range(0, 360f);
+
+        transform.rotation = Quaternion.Euler(xRot, yRot, 0);
+    }
+
+    IEnumerator Lightning()
+    {
+        StartCoroutine(Thunderise(Random.Range(0.5f, 3f)));
+
+        RandomDirect();
+
+
+        int numFlashes = Random.Range(1, 4);
+        float flashTimer = 0;
+        for (int i = 0; i < numFlashes - 1; i++)
+        {
+            //ON
+            flashTimer = Random.Range(0.1f, 0.3f);
+            light.intensity = MAX_INTENSITY;
+            while(flashTimer > 0)
+            {
+                flashTimer -= Time.deltaTime;
+                yield return null;
+            }
+
+            //OFFF
+            flashTimer = Random.Range(0.02f, 0.15f);
+            light.intensity = 0;
+            while (flashTimer > 0)
+            {
+                flashTimer -= Time.deltaTime;
+                yield return null;
+            }
+        }
+
+        //One more!
+        flashTimer = Random.Range(0.15f, 0.3f);
+        light.intensity = MAX_INTENSITY;
+        while (flashTimer > 0)
+        {
+            flashTimer -= Time.deltaTime;
+            yield return null;
+        }
+        //And fade!
+        float fadeTime = flashTimer = Random.Range(1, 2f);
+
+        while(flashTimer > 0)
+        {
+            light.intensity = MAX_INTENSITY*flashTimer / fadeTime;
+            flashTimer -= Time.deltaTime;
+            yield return null;
+        }
+        light.intensity = 0;
+        inProgresss = false;
+        HowLongTillLight();
+    }
+
+    //[Range(-10, 10)]
+    //[SerializeField] float v = 1;
+    IEnumerator Thunderise(float thunderTime)
+    {
+        EventInstance inst = RuntimeManager.CreateInstance(thunderEvent);
+
+        float vol = 0.5f - thunderTime; //[0, -2.5]
+
+        vol = Mathf.Exp(2*vol); //[0, something smalll but positive
+
+
+        print(thunderTime + " " + vol);
+
+        inst.setVolume(vol);
+        //float d;
+        //inst.getVolume(out d);
+        //print(d);
+
+        while (thunderTime > 0)
+        {
+            thunderTime -= Time.deltaTime;
+            yield return null;
+        }
+        inst.start();
+    }
+
+    void Unthunderise()
+    {
+        StopAllCoroutines();
+        inProgresss = false;
+    }
+}
--- README.md
+++
@@ -1,3 +0,0 @@
-# A tower defense game
-
-It's from https://assetstore.unity.com/packages/essentials/tutorial-projects/tower-defense-template-107692
